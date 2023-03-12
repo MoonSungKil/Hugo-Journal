063c53c78d6deb6043104395769c41b1b7c1e008
MoonSungKil <kris_donovski@hotmail.com>
2023-03-12 19:22:43 +0100

    adding main page

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,19 @@ window.addEventListener("scroll", () => {
     pdfBanner.classList.remove("show_pdf_banner");
   }
 });
+
+const clickRight = document.querySelector(".author_list_button_right");
+const clickLeft = document.querySelector(".author_list_button_left");
+
+const innerAuthorsList = document.querySelector(".authors_list");
+const outerAuthorsList = document.querySelector(".authors_list_container");
+
+let currentPosition = 0;
+const itemWidth = innerAuthorsList.children[0].offsetWidth;
+const maxPosition = -1 * (innerAuthorsList.children.length - 1) * itemWidth;
+
+let moveSection = 0;
+
+clickLeft.addEventListener("click", () => {});
+
+clickRight.addEventListener("click", () => {});
